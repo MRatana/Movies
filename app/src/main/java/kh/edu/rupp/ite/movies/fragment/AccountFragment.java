ef3f6050b98145adc76dd232534321d9aad3f731
MRatana <ratanamg105@gmail.com>
2023-11-02 17:45:39 +0700

    design layout(Download, Favorite, Explorer, and Account Fragment)

--- app/src/main/java/kh/edu/rupp/ite/movies/fragment/AccountFragment.java
+++ app/src/main/java/kh/edu/rupp/ite/movies/fragment/AccountFragment.java
@@ -1,2 +1,24 @@
-package kh.edu.rupp.ite.movies.fragment;public class AccountFragment {
+package kh.edu.rupp.ite.movies.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kh.edu.rupp.ite.movies.databinding.FragmentAccountBinding;
+
+public class AccountFragment extends Fragment {
+
+    public FragmentAccountBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 }
--- app/src/main/java/kh/edu/rupp/ite/movies/fragment/ExplorerFragment.java
+++ app/src/main/java/kh/edu/rupp/ite/movies/fragment/ExplorerFragment.java
@@ -1,2 +1,24 @@
-package kh.edu.rupp.ite.movies.fragment;public class ExplorFragment {
+package kh.edu.rupp.ite.movies.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import kh.edu.rupp.ite.movies.databinding.FragmentExplorerBinding;
+
+public class ExplorerFragment extends Fragment {
+
+    public FragmentExplorerBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentExplorerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 }
