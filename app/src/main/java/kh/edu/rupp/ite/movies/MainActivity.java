b4ab1f1f623fb7ee5dcc8e1de28a389dea6543d4
Soklayy <soklayiter34@gmail.com>
2023-11-02 11:26:41 +0700

    Homefragment

--- app/src/main/java/kh/edu/rupp/ite/movies/MainActivity.java
+++ app/src/main/java/kh/edu/rupp/ite/movies/MainActivity.java
@@ -1,14 +1,49 @@
 package kh.edu.rupp.ite.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import kh.edu.rupp.ite.movies.databinding.ActivityMainBinding;
+import kh.edu.rupp.ite.movies.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        showFragment(new HomeFragment());
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            return true;
+        });
+    }
+
+
+
+    private void showFragment(Fragment fragment){
+        // FragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // FragmentTransaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace fragment in lytFragment
+        fragmentTransaction.replace(R.id.lyFragment, fragment);
+
+        // Commit transaction
+        fragmentTransaction.commit();
     }
 }
